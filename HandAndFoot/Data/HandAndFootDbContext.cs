178ed6d34f550e0387cfa909c9020b935ce85b68
Gregory Dukes <kcils360@gmail.com>
2018-11-19 11:01:02 -0800

    added migrations and initial DB build

--- HandAndFoot/Data/HandAndFootDbContext.cs
+++ HandAndFoot/Data/HandAndFootDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HandAndFoot.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,8 @@ namespace HandAndFoot.Data
         {
 
         }
+
+        public DbSet<GameTable> GameTables { get; set; }
+        public DbSet<Player> Players { get; set; }
     }
 }
